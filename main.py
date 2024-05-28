87692f1d7451458e40d2d5ba65eb9e3c2fb04db1
André Biseth <andre@biseth.net>
2024-05-28 15:04:50 +0000

    Updated flask files

--- main.py
+++ main.py
@@ -1,54 +1,17 @@
-from flask import Flask, render_template
-
-class Car:
-
-    instanceCount: int = 0
-    cars = []
-
-    def __init__(self, brand: str = "Toyota", model: str = "Corolla", year: int = 2000, speed: int = 100) -> None:
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.speed = speed
-        Car.instanceCount += 1
-        Car.cars.append(self)
-
-    def __str__(self) -> str:
-        return f"This is a {self.brand} {self.model}, made in {self.year}, and can run in {self.speed} km/h"
-
-    def __repr__(self) -> str:
-        return f"Car('{self.brand}','{self.model}','{self.year}','{self.speed}')"
-    
-    def __del__(self):
-        print(f"Destroying the {self.brand}")
-        Car.instanceCount += 1
-        Car.cars.remove(self)
-
-    @classmethod
-    def printAvailableCars(cls):
-        for car in cls.cars:
-            print(car)
-
-    def increaseSpeed(self, speed: int):
-        self.speed += speed
-
-    def printSpeed(self):
-        print(f"Max speed is: {self.speed} km/h")
-
-    def printInstaceCount(self):
-        print(f"Instances: {Car.instanceCount}")
-
-
+from flask import Flask, redirect, render_template, request
+from models.Car import Car
 
 app = Flask("test")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/newcar')
-def new_car():
-    car = Car("Volvo", "V40", 1980, 98)
-    return car.__str__()
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/cars")
+def cars():
+    return Car.get_available_cars()
+
 
 app.run()
--- models/Car.py
+++ models/Car.py
@@ -3,11 +3,17 @@ class Car:
     instanceCount: int = 0
     cars = []
 
-    def __init__(self, brand: str = "Toyota", model: str = "Corolla", year: int = 2000, speed: int = 100) -> None:
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.speed = speed
+    def __init__(
+        self,
+        brand: str = "Toyota",
+        model: str = "Corolla",
+        year: int = 2000,
+        speed: int = 100,
+    ) -> None:
+        self.brand: str = brand
+        self.model: str = model
+        self.year: int = year
+        self.speed: int = speed
         Car.instanceCount += 1
         Car.cars.append(self)
 
@@ -16,41 +22,29 @@ class Car:
 
     def __repr__(self) -> str:
         return f"Car('{self.brand}','{self.model}','{self.year}','{self.speed}')"
-    
+
     def __del__(self):
         print(f"Destroying the {self.brand}")
         Car.instanceCount += 1
         Car.cars.remove(self)
 
     @classmethod
-    def printAvailableCars(cls):
+    def print_available_cars(cls):
         for car in cls.cars:
             print(car)
 
-    def increaseSpeed(self, speed: int):
+    @classmethod
+    def get_available_cars(cls):
+        result = []
+        for car in cls.cars:
+            result.append(car.__dict__)
+        return result
+
+    def increase_speed(self, speed: int):
         self.speed += speed
 
-    def printSpeed(self):
+    def print_speed(self):
         print(f"Max speed is: {self.speed} km/h")
 
-    def printInstaceCount(self):
+    def print_instace_count(self):
         print(f"Instances: {Car.instanceCount}")
-
-
-# volvo = Car("Volvo", "XC60", 1990, 98)
-# tesla = Car("Tesla", "Model 3", 2023, 200)
-# lada = Car("Lada", "2000")
-# ford = Car()
-# Car.printAvailableCars()
-
-
-# tesla.printSpeed()
-# tesla.increaseSpeed(10)
-# tesla.printSpeed()
-
-# del ford
-
-# print(volvo)
-# print(tesla)
-
-# print(volvo.__repr__())
